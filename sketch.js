3c232fcfafacb6fa35d3abbece9f47dd757a0792
Rob Duarte <me@robduarte.com>
2022-11-14 18:42:43 -0500

    First commit, before making a video demo

--- sketch.js
+++ sketch.js
@@ -1,7 +1,20 @@
+let div;
+
 function setup() {
   createCanvas(400, 400);
+  textSize(100);
+  div = createDiv('this');
+  div.class('neonText');
 }
 
 function draw() {
   background(125);
+  div.position(mouseX, mouseY);
+
+  //  text("this",20,100);
+
+}
+
+function mouseClicked() {
+  div.class('glow');
 }
